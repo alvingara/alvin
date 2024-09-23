a496d2d872e8b2793b4a176ac689e618768e7d4f
alvingara <alvinoriscaanggaraputra@gmail.com>
2024-09-23 21:49:50 +0700

    tes

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,23 @@
 export default function Home() {
   return (
     <>
-       
-      <h2 className="text-blue-600 text-2xl mt-16">HAIII</h2>
-
-      <p class="text-left ...">Perkenalkan saya Vino dari lp3i jakarta  .</p>
+      <div className="flex mt-16">
+                <div className="md:w-3/4 px-4 ">
+                    <div className="  rounded-xl">
+                    <h1 className="text-4xl font-semibold text-gray-900 dark:text-white">Hai</h1>
+      <h2 className="text-blue-600 text-3xl mt-2">Alvino</h2>
+      <p class="text-left ...">Perkenalkan saya Vino dari lp3i jakarta cita cita menjadi BIG BOSS.</p>
       <div>
-        <button className="mt-14 rounded-lg bg-sky-500/100 w-48 min-h-12 text-white">MULAI</button>
+      <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">MULAI</button>
       </div>
+                        
+                    </div>
+                </div>
+                <div className="md:w-1/3 border">
+                <img class="h-auto max-w-lg rounded-lg" src="/images/CR7.jpg" alt="image description">
+                </img>
+                </div>
+            </div>
     </>
   );
 }
